2531684bb9020f2fcdf5c16df09b1526955516e4
duha <tungha1310@gmail.com>
2024-11-19 14:47:02 +0200

    backup 19.11

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,21 @@
 /*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:59:40 by duha              #+#    #+#             */
-/*   Updated: 2024/11/18 17:03:33 by duha             ###   ########.fr       */
+/*   Updated: 2024/11/19 14:44:39 by duha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int	ft_printf(const char *, ...)
 {
 
 }
+
+// void va_start(va_list ap, last);
+// argument_type va_arg(va_list ap, type);
+// void va_copy(va_list dest, va_list src);
+// void va_end(va_list ap);
